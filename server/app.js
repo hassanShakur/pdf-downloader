520943266c706af0846cfafacc18dc09af1f4d80
hassanShakur <hass.shakurhassan@gmail.com>
2024-01-28 22:18:34 +0300

    add a basic home component to fetch backend data

--- server/app.js
+++ server/app.js
@@ -4,14 +4,16 @@ const cors = require('cors');
 const app = express();
 const port = 7000;
 
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+  res.send('Hello, world!');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
